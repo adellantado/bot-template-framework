715378277fa30a98dd2ec0d144d7708e1cbdb41c
Alex_Tymchenko <adellantado@gmail.com>
2018-07-23 18:58:01 +0300

    add ask validator

--- src/TemplateConversation.php
+++ src/TemplateConversation.php
@@ -19,6 +19,8 @@ class TemplateConversation extends Conversation {
      */
     public $engine;
 
+    public $callback;
+
     /**
      * @return BotMan
      */
@@ -37,38 +39,38 @@ class TemplateConversation extends Conversation {
                 }
             }
         }
-        $this->ask($question, [$this, 'askCallback']);
-    }
 
-    public function askCallback(Answer $answer) {
-        $block = $this->engine->setBot($this->bot)
-            ->getBlock($this->blockName);
+        $this->callback = function(Answer $answer) {
+            $block = $this->engine->setBot($this->bot)
+                ->getBlock($this->blockName);
 
-        if (array_key_exists('validate', $block)) {
-            $validator = new Validator();
-            if ($block['validate'] == 'number$' && !$validator->number($answer->getText())) {
-                $this->ask(Question::create($validator->errorNumberMsg()), [$this, 'callback']);
-                return;
-            } elseif ($block['validate'] == 'url' && !$validator->url($answer->getText())) {
-                $this->ask(Question::create($validator->errorUrlMsg()), [$this, 'callback']);
-                return;
-            } elseif ($block['validate'] == 'email' && !$validator->email($answer->getText())) {
-                $$this->ask(Question::create($validator->errorEmailMsg()), [$this, 'callback']);
-                return;
-            } else {
-                if (!$validator->regexp($block['validate'], $answer->getText())) {
-                    $this->ask(Question::create('Can\'t validate input'), [$this, 'callback']);
+            if (array_key_exists('validate', $block)) {
+                $validator = new Validator();
+                if ($block['validate'] == 'number$' && !$validator->number($answer->getText())) {
+                    $this->ask(Question::create($validator->errorNumberMsg()), $this->callback);
                     return;
+                } elseif ($block['validate'] == 'url' && !$validator->url($answer->getText())) {
+                    $this->ask(Question::create($validator->errorUrlMsg()), $this->callback);
+                    return;
+                } elseif ($block['validate'] == 'email' && !$validator->email($answer->getText())) {
+                    $$this->ask(Question::create($validator->errorEmailMsg()), $this->callback);
+                    return;
+                } else {
+                    if (!$validator->regexp($block['validate'], $answer->getText())) {
+                        $this->ask(Question::create('Can\'t validate input'), $this->callback);
+                        return;
+                    }
                 }
             }
-        }
 
-        if (array_key_exists('result', $block) && array_key_exists('save', $block['result'])) {
-            $this->engine->saveVariable($block['result']['save'], $answer->getText());
-        }
-        if (array_key_exists('next', $block)) {
-            $this->engine->executeNextBlock($block, $answer->getText());
-        }
+            if (array_key_exists('result', $block) && array_key_exists('save', $block['result'])) {
+                $this->engine->saveVariable($block['result']['save'], $answer->getText());
+            }
+            if (array_key_exists('next', $block)) {
+                $this->engine->executeNextBlock($block, $answer->getText());
+            }
+        };
+
+        $this->ask($question, $this->callback);
     }
-
 }
