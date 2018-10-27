ea6042814922ac0426c1d02225647c532d70bc83
Alex_Tymchenko <adellantado@gmail.com>
2018-10-27 17:11:30 +0300

    add methods for the validation of a driver limits

--- src/Helpers/Validator.php
+++ src/Helpers/Validator.php
@@ -53,4 +53,36 @@ class Validator {
         return 'Please, type your confirmation input with exact value (case sensitive)';
     }
 
+    public function scenarioWithDriverLimits(array $template) {
+        $drivers = $template['drivers'];
+        foreach ($template['blocks'] as $block) {
+            foreach ($drivers as $driver) {
+                $message = $this->blockWithDriverLimits($block, strtolower($driver['name']));
+                if ($message) {
+                    return $message;
+                }
+            }
+        }
+        return null;
+    }
+
+    public function blockWithDriverLimits(array $block, $driver) {
+        $type = $block['type'];
+        if ($driver == 'viber' && $type == 'image') {
+            if (array_key_exists('text', $block['content']) && mb_strlen($block['content']['text']) > 120) {
+                return 'For image blocks, Viber requires max = 120 characters';
+            }
+            if (!in_array(pathinfo($block['content']['url'], PATHINFO_EXTENSION), ['jpg', 'jpeg'])) {
+                return 'For image blocks, Viber requires only JPEG format';
+            }
+        } elseif ($driver == 'facebook' && $type == 'menu') {
+            foreach ($block['content']['buttons'] as $menu) {
+                if (count($menu) > 3) {
+                    return 'For menu blocks, Facebook requires max = 3 buttons in a menu';
+                }
+            }
+        }
+
+        return null;
+    }
 }
