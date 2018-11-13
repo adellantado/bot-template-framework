60407e43a6e1ae16fcadb4895caa6ea6b6435c07
Alex_Tymchenko <adellantado@gmail.com>
2018-11-13 15:30:13 +0200

    add save content in telegram driver

--- src/Distinct/Telegram/TelegramDriverExtended.php
+++ src/Distinct/Telegram/TelegramDriverExtended.php
@@ -3,9 +3,16 @@
 namespace BotTemplateFramework\Distinct\Telegram;
 
 
+use Symfony\Component\HttpFoundation\Request;
 use BotMan\Drivers\Telegram\TelegramDriver;
 
 class TelegramDriverExtended extends TelegramDriver {
+
+    public function buildPayload(Request $request)
+    {
+        $this->content = $request->getContent();
+        parent::buildPayload($request);
+    }
 
     public function messagesHandled() {
     }
