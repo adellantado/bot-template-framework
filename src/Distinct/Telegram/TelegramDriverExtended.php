f3cdadd5e30af1958a445e6c46773a1f77a5783b
Alex_Tymchenko <adellantado@gmail.com>
2019-01-18 23:40:05 +0200

    fix telegram phone payload

--- src/Distinct/Telegram/TelegramDriverExtended.php
+++ src/Distinct/Telegram/TelegramDriverExtended.php
@@ -2,7 +2,8 @@
 
 namespace BotTemplateFramework\Distinct\Telegram;
 
-
+use BotMan\BotMan\Messages\Incoming\IncomingMessage;
+use Illuminate\Support\Collection;
 use Symfony\Component\HttpFoundation\Request;
 use BotMan\Drivers\Telegram\TelegramDriver;
 
@@ -15,6 +16,34 @@ class TelegramDriverExtended extends TelegramDriver {
     }
 
     public function messagesHandled() {
+    }
+
+    public function loadMessages()
+    {
+        if ($this->payload->get('callback_query') !== null) {
+            $callback = Collection::make($this->payload->get('callback_query'));
+
+            $messages = [
+                new IncomingMessage($callback->get('data'), $callback->get('from')['id'],
+                    $callback->get('message')['chat']['id'], $callback->get('message')),
+            ];
+        } elseif ($this->payload->get('contact') !== null) {
+            $messages = [
+                new IncomingMessage($this->event->get('contact')['phone_number'], $this->event->get('from')['id'], $this->event->get('chat')['id'],
+                    $this->event),
+            ];
+        } elseif ($this->isValidLoginRequest()) {
+            $messages = [
+                new IncomingMessage('', $this->queryParameters->get('id'), $this->queryParameters->get('id'), $this->queryParameters),
+            ];
+        } else {
+            $messages = [
+                new IncomingMessage($this->event->get('text'), $this->event->get('from')['id'], $this->event->get('chat')['id'],
+                    $this->event),
+            ];
+        }
+
+        $this->messages = $messages;
     }
 
 }
