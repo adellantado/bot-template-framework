012ca0d2e5609550a0e72669909e40fd744f6187
Alex_Tymchenko <adellantado@gmail.com>
2019-05-13 15:36:02 +0300

    - add share buttons to facebook, viber, telegram

--- src/Strategies/ViberComponentsStrategy.php
+++ src/Strategies/ViberComponentsStrategy.php
@@ -115,7 +115,7 @@ class ViberComponentsStrategy implements IComponentsStrategy, IStrategy {
                 $schema = parse_url($callback, PHP_URL_SCHEME);
                 if (in_array($schema, ['mailto', 'http', 'https', 'tel', 'share'])) {
                     if ($schema == 'share') {
-                        $keyboard->addButton($title, 'open-url', 'viber://forward?text='.parse_url($callback, PHP_URL_HOST), $options['TextSize'] ?? 'regular', $options['BgColor'] ?? null, $width);
+                        $keyboard->addButton($title, 'open-url', 'viber://forward?text='.substr($callback, 8), $options['TextSize'] ?? 'regular', $options['BgColor'] ?? null, $width);
                     } else {
                         $keyboard->addButton($title, 'open-url', $callback, $options['TextSize'] ?? 'regular', $options['BgColor'] ?? null, $width);
                     }
